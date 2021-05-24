d3778a9a28827b475138865ae285f52e5e4a06e3
DentiQ <dentiq0414@gmail.com>
2021-05-25 02:58:25 +0900

    20210525

---
+++ week13/day1/1753.py
@@ -0,0 +1,58 @@
+"""
+https://www.acmicpc.net/problem/1753
+최단경로
+문제
+방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오.
+단, 모든 간선의 가중치는 10 이하의 자연수이다.
+
+입력
+첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다. (1≤V≤20,000, 1≤E≤300,000) 모든 정점에는 1부터 V까지 번호가 매겨져 있다고 가정한다.
+둘째 줄에는 시작 정점의 번호 K(1≤K≤V)가 주어진다. 셋째 줄부터 E개의 줄에 걸쳐 각 간선을 나타내는 세 개의 정수 (u, v, w)가 순서대로 주어진다.
+이는 u에서 v로 가는 가중치 w인 간선이 존재한다는 뜻이다. u와 v는 서로 다르며 w는 10 이하의 자연수이다. 서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다.
+
+출력
+첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다. 시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
+"""
+from sys import stdin
+import heapq
+
+input = stdin.readline
+
+V, E = map(int, input().split())
+S = int(input())
+
+graph = [[] for _ in range(V+1)]
+
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append([w, v])
+
+print(graph)
+
+def dijkstra(graph, start):
+    dists = [float('inf')]*(V+1)
+    dists[start] = 0
+    q = []
+    heapq.heappush(q, [0, start])
+
+    while q:
+        c_dist, c_dest = heapq.heappop(q)
+
+        if dists[c_dest] < c_dist:
+            continue
+
+        for n_dist, n_dest in graph[c_dest]:
+
+            dist = c_dist + n_dist
+            if dist < dists[n_dest]:
+                dists[n_dest] = dist
+                heapq.heappush(q, [dist, n_dest])
+
+    for i in range(1, V + 1):
+        if dists[i] == float('inf'):
+            print("INF")
+        else:
+            print(dists[i])
+
+
+dijkstra(graph, S)
